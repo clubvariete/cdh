c0cfa430615d9e2d585f1b3abd388ee63c5b8d15
clubvariete <nik@clubvariete.ch>
2021-04-07 08:25:26 +0000

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+const config = require('./.contentful.json')
+
+
 module.exports = {
   /*
   ** Headers of the page
@@ -37,3 +40,15 @@ module.exports = {
   }
 }
 
+
+
+module.exports = {
+  // ...
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  }
+  // ...
+}
